84d598fe9daef1b5aecc969dd6db7fb0f59f197c
ZanaGuda <zanaguda28@gmail.com>
2022-11-12 09:09:29 +0100

    basics in js

---
+++ script.js
@@ -0,0 +1,96 @@
+// Execute a km to mile converter. 1 km = 0,621371 miles. State this as a constant in the code.
+// Execute a calculator. A user types in two numbers and the program puts out the results of + - * / actions with those numbers.
+// A user types in the values of a and b for a * x + b = 0 formula and the program counts the x value.
+// An employee’s monthly paycheck is $250 + 10% from sales. Request the sum of all monthly sales and count the salary.
+
+// let kilometers = prompt("Kilometres in miles: ") * 1;
+// let miles = kilometers * 0.621371;
+
+// console.info(miles);
+
+// let number1 = prompt("First number: ") * 1;
+// let number2 = prompt("Second number") * 1;
+
+// let addition = number1 + number2;
+// let substract = number1 - number2;
+// let division = number1 / number2;
+// let multiply = number1 * number2;
+
+// console.info(addition);
+// console.info(substract);
+// console.info(division);
+// console.info(multiply);
+
+// let a = prompt("First number");
+// let b = prompt("Second number");
+// let x = -b / a;
+
+// console.info(x);
+
+// let sales = prompt("Enter sales") * 1;
+// let yearlySales = sales * 12;
+// let salaryYear = 250 * 12;
+// let salary = salaryYear + 0.1 * yearlySales;
+// console.info(salary);
+
+// Request a user’s name and display the response “Hello, [name]”.
+// Request a user’s year of birth, count his age, and display the result. Store the current year as a constant.
+// Request a length of a side of a square from a user and display the perimeter of the square.
+// Request a radius of a circle and display the area of such a circle.
+// Request a distance in km between the two cities from a user, and the time they want to cover it within. Count the speed needed to be on time.
+// Create a currency converter. A user types in USD and the program converts them to EUR. The exchange rate should be stored as a constant.
+// A user states a flash drive memory in GB. The program has to count how many 820MB files can be stored on this flash drive.
+// A user types in an amount of money in a wallet and the price of one chocolate bar. The program counts how many chocolate bars the user can buy, and how much change will he/she have left.
+// The user types in a sum of bank deposits for 2 months with a yearly interest rate of 5%. The program counts the sum of interest.
+
+// let name = prompt("Enter your name: ");
+// let greeting = "Hello" + " " + name;
+// console.info(greeting);
+
+// const currentYear = 2022;
+// let userBirthDate = prompt("Enter the year you were born: ") * 1;
+// let result = currentYear - userBirthDate;
+// console.info("You are: " + result);
+
+// let sideLength = prompt("Enter the side length: ");
+// let perimeter = sideLength * 4;
+// console.info(
+//   "Perimetri i katrorit me brinje: " + sideLength + " eshte " + perimeter
+// );
+
+// let radius = prompt("Enter radius: ") * 1;
+// let areaOfCircle = 2 * 3.14 * radius;
+// console.info(areaOfCircle);
+
+// let distance = prompt("Distance in km: ") * 1;
+// let time = prompt("Enter time: ");
+// let speed = distance / time;
+// console.info(speed);
+
+// let dollar = prompt("Enter amount in dollar: ");
+// const exchange = 0.98;
+// const euro = dollar * exchange;
+// console.info(euro);
+
+// let GB = prompt("Enter GB:");
+// let MB = (GB * 1000) / 820;
+// console.info("You can store: " + MB);
+
+// let money = prompt("Enter amount of money: ") * 1;
+// let chocolate = prompt("Enter price of one chocolate: ") * 1;
+// let amountBought = money / chocolate;
+// let left = money - chocolate * amountBought;
+// console.info(
+//   "With " +
+//     money +
+//     " you can buy " +
+//     amountBought +
+//     " chocolate" +
+//     "and have " +
+//     left +
+//     " money left"
+// );
+// let sum = prompt('Sum: ');
+// let interestperMonth = 0.05/12;
+// let interest = interestperMonth*2;
+// console.log(interest*sum);
